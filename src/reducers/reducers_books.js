fc42ddeae2fa80a46d910ae6f8e4bed3d284454e
ywCN <wangyouhao90@gmail.com>
2017-12-25 18:56:27 -0500

    add page property

--- src/reducers/reducers_books.js
+++ src/reducers/reducers_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'book a'},
-        {title: 'book b'},
-        {title: 'book c'},
-        {title: 'book d'}
+        {title: 'book a', page: 110 },
+        {title: 'book b', page: 121 },
+        {title: 'book c', page: 312 },
+        {title: 'book d', page: 222 }
     ];
 }
