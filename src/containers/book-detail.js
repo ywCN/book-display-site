469b9b1ba1cb6d06d88a3fc4de6913b1918a6cb4
ywCN <wangyouhao90@gmail.com>
2017-12-25 18:48:14 -0500

    connect reducer and container

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { activeBook } from '../actions/index';
 
-
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render() {
         return (
             <div>Book Detail!</div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        book: state.activeBook // activeBook is from index. activeBook: ActiveBook
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail);
